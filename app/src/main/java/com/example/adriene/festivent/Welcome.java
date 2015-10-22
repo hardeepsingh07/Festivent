f05b8fa492eb989b689fe9a070d2608595f859d4
Hardeep Singh <hardeep07@yahoo.com>
2015-10-22 15:21:57 -0700

    Timer for the Welcome Activity to move to Main Activity

--- app/src/main/java/com/example/adriene/festivent/Welcome.java
+++ app/src/main/java/com/example/adriene/festivent/Welcome.java
@@ -1,13 +1,26 @@
 package com.example.adriene.festivent;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
 
 public class Welcome extends AppCompatActivity {
 
+    public static final int TIME = 2000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+
+        //Timer to move to Main Activity
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(Welcome.this, Main.class);
+                startActivity(i);
+                finish();
+            }
+        }, TIME);
     }
 }
