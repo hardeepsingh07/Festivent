49034e2ca7ab7ee3bae2d682956b41c3bba35067
Hardeep Singh <hardeep07@yahoo.com>
2015-12-03 17:56:21 -0800

    Settings page perseve the UI state

--- app/src/main/java/com/example/adriene/festivent/Settings.java
+++ app/src/main/java/com/example/adriene/festivent/Settings.java
@@ -18,6 +18,9 @@ public class Settings extends AppCompatActivity {
     public SharedPreferences prefs;
     public boolean switchState;
     public String indexMiles, indexTime;
+    public String [] times = {"1 Day", "2 Days", "3 Days", "4 Days", "1 Week",
+                                    "2 Weeks", "1 Month", "2 Months"};
+    public String [] radius = {"10 Miles", "25 Miles", "50 Miles", "100 Miles"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,34 +30,38 @@ public class Settings extends AppCompatActivity {
 
         prefs = PreferenceManager.getDefaultSharedPreferences(Settings.this);
         mySwitch = (Switch) findViewById(R.id.switch_default);
-        /*if(prefs.getString("dialogChoice", "") == "map") {
+
+        if(prefs.getString("dialogChoice", "") == "map") {
             mySwitch.setChecked(true);
-        } else {
+        } else if(prefs.getString("dialogChoice", "") == "list") {
             mySwitch.setChecked(false);
-        }*/
+        }
 
         mySwitch.setOnCheckedChangeListener(new OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView,
                                          boolean isChecked) {
                 if (isChecked) {
-                    prefs.edit().putString("dialogChoice", "map").commit();
+                    prefs.edit().putString("dialogChoice", "map").apply();
                 } else {
-                    prefs.edit().putString("dialogChoice", "list").commit();
+                    prefs.edit().putString("dialogChoice", "list").apply();
                 }
 
             }
         });
-        Spinner spinnerT = (Spinner) findViewById(R.id.spinner_time);
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
-                R.array.time_array, android.R.layout.simple_spinner_item);
+        final Spinner spinnerT = (Spinner) findViewById(R.id.spinner_time);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, times);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerT.setAdapter(adapter);
+
+        spinnerT.setSelection(prefs.getInt("timeSelection", 0));
         spinnerT.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                int index = spinnerT.getSelectedItemPosition();
+                prefs.edit().putInt("timeSelection", index).apply();
                 String selected = parent.getItemAtPosition(position).toString();
-                prefs.edit().putString("time", selected).commit();
+                prefs.edit().putString("time", selected).apply();
             }
 
             @Override
@@ -63,16 +70,19 @@ public class Settings extends AppCompatActivity {
             }
         });
 
-        Spinner spinnerR = (Spinner) findViewById(R.id.spinner_radius);
-        ArrayAdapter<CharSequence> adapter1 = ArrayAdapter.createFromResource(this,
-                R.array.radius_array, android.R.layout.simple_spinner_item);
+        final Spinner spinnerR = (Spinner) findViewById(R.id.spinner_radius);
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, radius);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinnerR.setAdapter(adapter1);
+
+        spinnerR.setSelection(prefs.getInt("radiusSelection", 0));
         spinnerR.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                int index = spinnerR.getSelectedItemPosition();
+                prefs.edit().putInt("radiusSelection", index).apply();
                 String selected = parent.getItemAtPosition(position).toString();
-                prefs.edit().putString("miles", selected).commit();
+                prefs.edit().putString("miles", selected).apply();
             }
 
             @Override
